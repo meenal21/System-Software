4f306755fe9efa448a66e6d97f0a9f711063b74f
meenal21 <mpbjain@gmail.com>
2024-09-12 12:10:14 +0530

    8c.c

--- hands_on_list_2/8c.c
+++ hands_on_list_2/8c.c
@@ -1,6 +1,16 @@
+/*
+================================================================================
+Name: 8c.c
+Author: Meenal Jain
+Description: Write a seperate program using signal system call to catch the following signals.
+c. SIGFPE
+Date: 12th September 2024
+================================================================================
+*/
 #include <stdio.h>
 #include <unistd.h>
 #include <signal.h>
+#include <stdlib.h>
 
 void fpeHandler(int sig);
 
@@ -9,11 +19,14 @@ int main(){
 	signal(SIGFPE, fpeHandler);
 	int x;
 	x = 5/0;
-	printf("Caught the SIGFPE signal!");	
+	printf("Caught the SIGFPE signal!\n");	
 	return 0;
 }
 
 void fpeHandler(int sig){
-	printf("Arithmetic issue has been caught!");
+	printf("Arithmetic issue has been caught!\n");
 	exit(0);
 }
+/*
+Arithmetic issue has been caught!
+*/
