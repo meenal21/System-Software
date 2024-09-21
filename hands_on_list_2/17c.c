b02906eb086cfeb9b1997645b650bf63ded6a157
meenal21 <mpbjain@gmail.com>
2024-09-21 20:34:46 +0530

    17.c

---
+++ hands_on_list_2/17c.c
@@ -0,0 +1,80 @@
+/*
+================================================================================
+Name: 17c.c
+Author: Meenal Jain
+Description: Write a program to execute ls -l | wc.
+c. use fcntl
+Date: 21st September 2024
+================================================================================
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main() {
+    int pipefd[2]; // Pipe file descriptors
+    pid_t pid1, pid2;
+
+    // Create a pipe
+    if (pipe(pipefd) == -1) {
+        perror("pipe");
+        exit(EXIT_FAILURE);
+    }
+
+    // First child: Execute 'ls -l'
+    if ((pid1 = fork()) == -1) {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    }
+
+    if (pid1 == 0) { // Child process
+        // Use fcntl to duplicate the write end
+        fcntl(pipefd[1], F_DUPFD, STDOUT_FILENO);
+        close(pipefd[0]); // Close unused read end
+        close(pipefd[1]); // Close original write end
+
+        execlp("ls", "ls", "-l", NULL);
+        perror("execlp ls"); // Only reached if execlp fails
+        exit(EXIT_FAILURE);
+    }
+
+    // Second child: Execute 'wc'
+    if ((pid2 = fork()) == -1) {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    }
+
+    if (pid2 == 0) { // Child process
+        // Use fcntl to duplicate the read end
+        fcntl(pipefd[0], F_DUPFD, STDIN_FILENO);
+        close(pipefd[0]); // Close original read end
+        close(pipefd[1]); // Close unused write end
+
+        execlp("wc", "wc", NULL);
+        perror("execlp wc"); // Only reached if execlp fails
+        exit(EXIT_FAILURE);
+    }
+
+    // Parent process: close both ends of the pipe
+    close(pipefd[0]);
+    close(pipefd[1]);
+
+    // Wait for both children to finish
+    wait(NULL);
+    wait(NULL);
+
+    return 0;
+}
+/*
+
+total 412
+-rw-rw-r-- 1 ubuntu ubuntu   968 Sep 21 20:19 10a.c
+-rw-rw-r-- 1 ubuntu ubuntu  1023 Sep 21 20:18 10b.c
+-rw-rw-r-- 1 ubuntu ubuntu   968 Sep 21 20:17 10.c
+-rw-rw-r-- 1 ubuntu ubuntu   983 Sep 21 20:19 10c.c
+-rw-rw-r-- 1 ubuntu ubuntu  1323 Sep 21 20:24 11.c
+*/
